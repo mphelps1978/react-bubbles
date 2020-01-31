79d43bd7f53710e1ebf11c9bd0bf8ead2dc5b9c2
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-01-31 10:59:54 -0600

    PUT request compelte - colors update

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,9 +18,19 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+    .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log('Result: ', res)
+      updateColors([
+        ...colors.filter(color=> color.id !== colorToEdit.id),
+        res.data
+      ])
+      setEditing(false)
+    })
+    .catch(err => console.log(err)
+    )
+
   };
 
   const deleteColor = color => {
