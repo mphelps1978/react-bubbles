9a3c495b07df00a2dc86e08a3e364f26024dfbfe
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-01-31 10:53:41 -0600

    Login and Display Functionality complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage"
+import PrivateRoute from "./components/PrivateRoute"
 import "./styles.scss";
 
 function App() {
@@ -9,10 +11,14 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+        {/*
+          Build a PrivateRoute component that will
+          display BubblePage when you're authenticated
         */}
+          <Switch>
+            <PrivateRoute exact path = "/bubbles" component = {BubblePage} />
+            <Route component = {Login} />
+          </Switch>
       </div>
     </Router>
   );
